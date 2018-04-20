a554c919f97e93f2c8247675342a74465245deb1
Lorenzo Ibay <31312773+renzo324@users.noreply.github.com>
2018-04-20 15:17:29 +0800

    fixed readability

--- index.php
+++ index.php
@@ -6,8 +6,8 @@
     if ( have_posts() ) :
         while ( have_posts() ) :
             the_post();
-// the_title();
-the_content();
+            // the_title();
+            the_content();
         endwhile; // end while
     endif; // end if
 ?>
