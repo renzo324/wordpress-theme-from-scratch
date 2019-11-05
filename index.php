848a2cfdcf2b4e14a76d2481bb4ecdc1662e7ca2
Lorenzo Ibay <finnthehuman324@gmail.com>
2019-11-05 23:06:45 +0800

    remove modal

--- index.php
+++ index.php
@@ -54,26 +54,7 @@ if ($loop -> have_posts()) {
         $output .= get_the_content();
 		echo $output;   
     }
-    echo '
-    <div class="modal fade" id="landingPopUp" tabindex="-1" role="dialog" aria-labelledby="landingPopUpTitle" aria-hidden="true" style="display: block;">
-    <div class="modal-dialog modal-dialog-centered" role="document">
-      <div class="modal-content">
-        <div class="modal-header">
-          <h5 class="modal-title" id="landingPopUpTitle">Modal title</h5>
-          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-            <span aria-hidden="true">&times;</span>
-          </button>
-        </div>
-        <div class="modal-body">
-          ...
-        </div>
-        <div class="modal-footer">
-          <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-          <button type="button" class="btn btn-primary">Save changes</button>
-        </div>
-      </div>
-    </div>
-  </div> ';
+    
 }
 
 ?>
